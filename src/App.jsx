7859264333578a9a350e958069c2f3589c7fbe38
Eric Philippot <EricpDev@gmail.com>
2023-10-02 01:19:00 -0500

    updated the state object properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
 
-  return (
-    <>
+  constructor() {
+    super();
+
+    this.state = {
+      name: {firstName: "Eric", lastName: "Philippot"},
+    }
+  }
+
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <img src={viteLogo} className="logo" alt="vite logo" />
+
+        <p>Hello {this.state.name.firstName} {this.state.name.lastName}</p>
+
+        <button onClick={ () => {
+          this.setState(() => {
+            return {
+              name: {firstName: "Bogie", lastName: "Bear" }
+            }
+          }, () => console.log(this.state))
+        }}>
+          Change Name
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
