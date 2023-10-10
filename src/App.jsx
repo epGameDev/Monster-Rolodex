01893a6d27c45ab92a135dbc13e1c9719e2ef936
Eric Philippot <EricpDev@gmail.com>
2023-10-09 23:55:27 -0500

    added search filter

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,64 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {firstName: "James", lastName: "Sully"},
-        {firstName: "Mike", lastName: "Wazowski"},
-        {firstName: "Roz", lastName: "Peterson"},
+      monsters: [],
+      searchInput: "",
+    };
 
-      ]
-    }
+    console.log("Constructor Initialized");
   }
 
-  render() {
+  componentDidMount() {
+    console.log("Component Mounted");
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+      .catch((error) =>
+        console.log("Oops, There is an error in App.js, line 15 -26", error)
+      );
+  }
+
+  onInputChange = (event) => {
+    const searchInput = event.target.value.toLowerCase();
+    this.setState( () => {
+      return { searchInput };
+    })
+  }
+
+  render() 
+  {
+    console.log("Rendering Dom");
+
+    const { monsters, searchInput } = this.state
+    const { onInputChange } = this;
+    
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchInput);
+    });
+
+
     return (
       <div>
-        {
-          this.state.monsters.map((monster) => {
-            console.log(<h3 key={monster.id}>{monster.firstName} {monster.lastName}</h3>)
-            return <h3 key={monster.id}>{monster.firstName} {monster.lastName}</h3>
-          })
-        }
+        <input type="search" name="search" id="search__input" placeholder="Search Monsters" onChange={onInputChange} />
+
+        {filteredMonsters.map((monster) => {
+          return (
+            <h3 key={monster.id}> {monster.name} {monster.email} </h3>
+          );
+        })}
       </div>
     );
   }
