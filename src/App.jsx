42d4c0fd7902c9dc14e733836621aa645e05b2c8
Eric Philippot <EricpDev@gmail.com>
2024-01-15 16:43:25 -0600

    changed app class to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,45 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
+
 import CardList  from "./components/card-list/card-list.component"
 import SearchBar from "./components/search-bar/search-bar.component";
+
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      monsters: [],
-      searchInput: "",
-    };
+const App = () => {
+  // Setting state values...
+  const [inputValue, setInputValue] = useState("");
+  const [monstersArray, setMonsterArrayValue] = useState([]);
+  const [filteredMonstersArray, setFilteredMonsterValue] = useState(monstersArray);
 
-  }
+  // Fetching Monsters API...
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((monster) => setMonsterArrayValue(monster));
+  });
 
-  componentDidMount() {
-      fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            // console.log(this.state);
-          }
-        )
-      )
-      .catch((error) =>
-        console.log("Oops, There is an error in App.js, line 15 -26", error)
-      );
-  }
+  // Creating filtered array as to not modify original array...
+  useEffect(() => {
+    const filteredMonsters = monstersArray.filter(monster => monster.name.toLowerCase().includes(inputValue));
+    setFilteredMonsterValue(filteredMonsters);
+  }, [monstersArray, inputValue]);
 
-  onInputChange = (event) => {
+  // Grabbing input from <SearchBar /> to filter array...
+  const onInputChange = event => {
     const searchInput = event.target.value.toLowerCase();
-    this.setState( () => {
-      return { searchInput };
-    })
+    setInputValue(searchInput);
   }
 
-  render() 
-  {
-    const { monsters, searchInput } = this.state
-    const { onInputChange } = this;
-    
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchInput);
-    });
+  return (
+    <div>
+      <h1 className="app__title">Monster&apos;s Rolodex</h1>
+      <SearchBar onChangeHandler={onInputChange} placeholder={"Search Monster"} className={"search__box-monster"}/>
 
-
-    return (
-      <div>
-        <h1 className="app__title">Monster&apos;s Rolodex</h1>
-        <SearchBar onChangeHandler={onInputChange} placeholder={"Search Monster"} className={"search__box-monster"}/>
-
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+      <CardList monsters={filteredMonstersArray} />
+    </div>
+  );
 }
 
 export default App;
